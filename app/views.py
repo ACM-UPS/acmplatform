7a3e7260959098991f61f417bddc005e979a14cb
Thavy Thach <thavythach@gmail.com>
2016-11-12 00:21:33 -0800

    (#35) calendar changed to python w/ markupsafe and jinja2 implementation

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, redirect, session, url_for, request, g, jsonify, Flask
+from flask import render_template, flash, redirect, session, url_for, request, g, jsonify, Flask, Markup
 from flask_login import login_user, logout_user, current_user, login_required
 from app import app, db
 from .models import User
@@ -11,5 +11,7 @@ import datetime
 @app.route('/')
 @app.route('/index')
 def index():
+
+	calendar = Markup("<iframe src=\"https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%2366cccc&amp;src=acm%40pugetsound.edu&amp;color=%231B887A&amp;ctz=America%2FLos_Angeles\" style=\"border-width:0\" width=\"800\" height=\"600\" frameborder=\"0\" scrolling=\"no\"></iframe>")
 	'''index route (main page)'''
-	return render_template('calendar.html')
+	return render_template('index.html', calendar=calendar)
