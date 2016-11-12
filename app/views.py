dcb144f8201586cfdb2e15fbf49dd5d0cd9206c5
Woo Jung <wooseokjung0826@gmail.com>
2016-11-12 01:43:21 -0800

    edited navbar fixed navbar page scroll margins swapped calendar | safe with flask block content

--- app/views.py
+++ app/views.py
@@ -10,6 +10,5 @@ import datetime
 
 @app.route('/')
 def index():
-	calendar = Markup("<iframe src=\"https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%2366cccc&amp;src=acm%40pugetsound.edu&amp;color=%231B887A&amp;ctz=America%2FLos_Angeles\" style=\"border-width:0\" width=\"800\" height=\"600\" frameborder=\"0\" scrolling=\"no\"></iframe>")
 	'''index route (main page)'''
-	return render_template('index.html', calendar=calendar)
+	return render_template('calendar.html')
