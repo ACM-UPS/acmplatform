353f7e9db2cec79c87cf3ad41769ff461fc2a1d9
Jesse Jenks <jjenks@ups.edu>
2016-11-13 19:09:41 -0800

    added blog page

--- app/models.py
+++ app/models.py
@@ -9,3 +9,14 @@ class User(db.Model):
 	def __repr__(self):
 		'''toString()'''
 		return '<user %r>' % (self.nickname)
+
+class BlogPost(db.Model):
+	'''Blog Post Model'''
+	id = db.Column(db.Integer, primary_key=True)
+	title = db.Column(db.String(120), index=True, unique=True)
+	content = db.Column(db.Text(), index=True)
+
+	def __init__(self, id, title, content):
+		self.id = id
+		self.title = title
+		self.content = content
--- app/views.py
+++ app/views.py
@@ -1,7 +1,8 @@
 from flask import render_template, flash, redirect, session, url_for, request, g, jsonify, Flask, Markup
 from flask_login import login_user, logout_user, current_user, login_required
-from app import app, db
-from .models import User
+from app import app, db, models
+# from .models import User
+# from .models import BlogPost
 import json
 import datetime
 
@@ -12,3 +13,24 @@ import datetime
 def index():
 	'''index route (main page)'''
 	return render_template('index.html')
+
+@app.route('/blog/')
+def blog():
+	return render_template('blog.html', post = models.BlogPost.query.all())
+
+@app.route('/blog/new/', methods = ['GET', 'POST'])
+def new():
+   if request.method == 'POST':
+      if not request.form['title'] or not request.form['content']:
+         flash('Please enter all the fields', 'error')
+      else:
+         post = models.BlogPost(id=777, title=request.form['title'], content=request.form['content'])
+         db.session.add(post)
+         db.session.commit()
+         flash('Record was successfully added')
+         return redirect(url_for('blog'))
+   return render_template('blog/new.html')
+
+# if __name__ == '__init__':
+#    db.create_all()
+#    app.run(debug = True)
