339f99b8ba809bfbb9db434121a4b73e5e57aed1
Thavy Thach <thavythach@gmail.com>
2016-11-26 11:35:15 -0800

    (#24) flask-admin added (#58) half the blog added - index not added - other stuff futureproof now - need to fix models.py

--- app/__init__.py
+++ app/__init__.py
@@ -5,13 +5,20 @@ from config import basedir,SQLALCHEMY_DATABASE_URI,SECRET_KEY
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
 
-
 app = Flask(__name__, static_url_path='', static_folder='static')
+
+from app.blog.views import mod
+app.register_blueprint(blog.views.mod, url_prefix='/blog')
+
 app.config.from_object('config')
 app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
 app.config['SECRET_KEY'] = SECRET_KEY
 db = SQLAlchemy(app)
 admin = Admin(app)
 
-from app import views, models
-admin.add_view(ModelView(models.User,db.session))
+db.create_all()
+
+from app import views, models as UserModel
+from app.blog import models as BlogModel
+admin.add_view(ModelView(UserModel.User,db.session))
+admin.add_view(ModelView(UserModel.BlogPost,db.session))
