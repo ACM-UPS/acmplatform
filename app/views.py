549c96fc9c80a405875e8ea51e3128988b14d110
Woo Jung <wooseokjung0826@gmail.com>
2017-02-16 18:38:40 -0800

    removed .html from url

--- app/views.py
+++ app/views.py
@@ -6,18 +6,18 @@ from .models import User
 
 
 @app.route('/')
-@app.route('/index.html') # TODO: REMOVE LATER FROM HTML
+@app.route('/index') # TODO: REMOVE LATER FROM HTML
 def index():
 	'''index route (main page)'''
 	return render_template('index.html')
 
 @app.route('/about')
 def about():
-	return render_template('about.html') # TODO replace w/ 'about.html'
+	return render_template('about') # TODO replace w/ 'about.html'
 
 @app.route('/team')
 def team():
-	return render_template('team.html') # TODO replace w/ 'team.html'
+	return render_template('team') # TODO replace w/ 'team.html'
 
 @app.route('/<var>')
 def pageNotFound(var):
@@ -26,11 +26,11 @@ def pageNotFound(var):
 
 # ALL BELOW NEED REVAMP
 
-@app.route('/blog-leftsidebar') 
+@app.route('/blog-leftsidebar')
 def blog_leftsidebar():
 	return render_template('blog-leftsidebar.html')
 
-@app.route('/blog-rightsidebar') 
+@app.route('/blog-rightsidebar')
 def blog_rightsidebar():
 	return render_template('blog-rightsidebar.html')
 
@@ -44,50 +44,50 @@ def right_sidebar():
 
 # below more revamp
 
-@app.route('/comingsoon') 
+@app.route('/comingsoon')
 def comingsoon():
 	return render_template('comingsoon.html')
 
-@app.route('/components.html') 
+@app.route('/components.html')
 def components():
 	return render_template('components.html')
 
-@app.route('/contact.html') # TODO Remove ".html"
+@app.route('/contact') # TODO Remove ".html"
 def contact():
 	return render_template('contact.html')
 
-@app.route('/fullwidth.html') # TODO Remove ".html"
+@app.route('/fullwidth') # TODO Remove ".html"
 def fullwidth():
 	return render_template('fullwidth.html')
 
-@app.route('/login.html') # TODO Remove ".html"
+@app.route('/login') # TODO Remove ".html"
 def login():
 	return render_template('login.html')
 
-@app.route('/portfolio.html') # TODO Remove ".html"
+@app.route('/portfolio') # TODO Remove ".html"
 def portfolio():
 	return render_template('portfolio.html')
 
-@app.route('/post-leftsidebar.html') # TODO Remove ".html"
+@app.route('/post-leftsidebar') # TODO Remove ".html"
 def post_leftsidebar():
 	return render_template('post-leftsidebar.html')
 
-@app.route('/post-rightsidebar.html') # TODO Remove ".html"
+@app.route('/post-rightsidebar') # TODO Remove ".html"
 def post_rightsidebar():
 	return render_template('post-rightsidebar.html')
 
-@app.route('/pricing-box.html') # TODO Remove ".html"
+@app.route('/pricing-box') # TODO Remove ".html"
 def pricing_box():
 	return render_template('pricing-box.html')
 
-@app.route('/register.html') # TODO Remove ".html"
+@app.route('/register') # TODO Remove ".html"
 def register():
 	return render_template('register.html')
 
-@app.route('/search-result.html') # TODO Remove ".html"
+@app.route('/search-result') # TODO Remove ".html"
 def search_result():
 	return render_template('search-result.html')
 
-@app.route('/typography.html')  # TODO Remove ".html"
+@app.route('/typography')  # TODO Remove ".html"
 def typography():
 	return render_template('typography.html')
