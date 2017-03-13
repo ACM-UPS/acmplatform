7c4867e55d8f3174c9259add24fe17479e37f0ad
Woo Jung <wooseokjung0826@gmail.com>
2017-03-13 03:21:07 -0700

    (#19) Implemented Contact Us page. Edit sender and receiver email before merging

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,16 @@
 # Example
-# from flask_wtf import Form
+from flask_wtf import Form
+from wtforms import TextField, TextAreaField, SubmitField, validators, ValidationError
 # from wtforms import StringField, BooleanField
 # from wtforms.validators import DataRequired, Length
 
 # class LoginForm(Form):
 # 	openid = StringField('openid', validators=[DataRequired()])
 # 	remember_me = BooleanField('remember_me', default=False)
+
+class ContactForm(Form):
+    name = TextField("Name", [validators.Required("Please enter your name")])
+    email = TextField("Email", [validators.Required("Please enter your email"), validators.Email("Please enter your email")])
+    subject = TextField("Subject", [validators.Required("Please enter a subject")])
+    message = TextAreaField("Message", [validators.Required("Please enter a message")])
+    submit = SubmitField("Send Message")
--- app/views.py
+++ app/views.py
@@ -1,23 +1,34 @@
 from flask import render_template, flash, redirect, session, url_for, request, g, jsonify, Flask, Markup
 from app import app, db
 from .models import User
+from forms import ContactForm
+from flask.ext.mail import Message, Mail
 
 # index view function supressed for brevity
 
+mail = Mail()
+
+app.config["MAIL_SERVER"] = "smtp.gmail.com"
+app.config["MAIL_PORT"] = 465
+app.config["MAIL_USE_SSL"] = True
+app.config["MAIL_USERNAME"] = 'SENDER@EMAIL.COM'
+app.config["MAIL_PASSWORD"] = 'PASSWORD'
+
+mail.init_app(app)
 
 @app.route('/')
-@app.route('/index') # TODO: REMOVE LATER FROM HTML
+@app.route('/index')
 def index():
 	'''index route (main page)'''
 	return render_template('index.html')
 
 @app.route('/about')
 def about():
-	return render_template('about.html') # TODO replace w/ 'about.html'
+	return render_template('about.html')
 
 @app.route('/team')
 def team():
-	return render_template('team.html') # TODO replace w/ 'team.html'
+	return render_template('team.html')
 
 @app.route('/<var>')
 def pageNotFound(var):
@@ -34,11 +45,11 @@ def blog_leftsidebar():
 def blog_rightsidebar():
 	return render_template('blog-rightsidebar.html')
 
-@app.route('/post-leftsidebar') # TODO Remove ".html"
+@app.route('/post-leftsidebar')
 def left_sidebar():
 	return render_template('post-leftsidebar.html')
 
-@app.route('/post-rightsidebar') # TODO Remove ".html"
+@app.route('/post-rightsidebar')
 def right_sidebar():
 	return render_template('post-rightsidebar.html')
 
@@ -52,42 +63,56 @@ def comingsoon():
 def components():
 	return render_template('components.html')
 
-@app.route('/contact') # TODO Remove ".html"
+@app.route('/contact', methods=['GET', 'POST'])
 def contact():
-	return render_template('contact.html')
+	form = ContactForm()
+	if request.method == 'POST':
+		if form.validate() == False:
+			flash('All field are required.')
+			return render_template('contact.html', form=form)
+		else:
+			msg = Message(form.subject.data, sender='SENDER@EMAIL.COM', recipients=['RECEIVER@EMAIL.COM'])
+			msg.body = """
+			From : %s <%s>
+			%s
+			""" % (form.name.data, form.email.data, form.message.data)
+			mail.send(msg)
+			return render_template('contact.html', success=True)
+	elif request.method == 'GET':
+		return render_template('contact.html', form=form)
 
-@app.route('/fullwidth') # TODO Remove ".html"
+@app.route('/fullwidth')
 def fullwidth():
 	return render_template('fullwidth.html')
 
-@app.route('/login') # TODO Remove ".html"
+@app.route('/login')
 def login():
 	return render_template('login.html')
 
-@app.route('/portfolio') # TODO Remove ".html"
+@app.route('/portfolio')
 def portfolio():
 	return render_template('portfolio.html')
 
-@app.route('/post-leftsidebar') # TODO Remove ".html"
+@app.route('/post-leftsidebar')
 def post_leftsidebar():
 	return render_template('post-leftsidebar.html')
 
-@app.route('/post-rightsidebar') # TODO Remove ".html"
+@app.route('/post-rightsidebar')
 def post_rightsidebar():
 	return render_template('post-rightsidebar.html')
 
-@app.route('/pricing-box') # TODO Remove ".html"
+@app.route('/pricing-box')
 def pricing_box():
 	return render_template('pricing-box.html')
 
-@app.route('/register') # TODO Remove ".html"
+@app.route('/register')
 def register():
 	return render_template('register.html')
 
-@app.route('/search-result') # TODO Remove ".html"
+@app.route('/search-result')
 def search_result():
 	return render_template('search-result.html')
 
-@app.route('/typography')  # TODO Remove ".html"
+@app.route('/typography')
 def typography():
 	return render_template('typography.html')
